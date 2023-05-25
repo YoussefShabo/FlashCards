a1dbda9a12c76108bc781f85eeb3c68352b53fd2
YoussefShabo <youssefshabo@gmail.com>
2023-05-25 20:42:28 +0200

    added the rest of the functions in the views.py

--- flash/website/views.py
+++ flash/website/views.py
@@ -102,7 +102,7 @@ def subtract(request):
     )
 
 def multiply(request):
- from random import randint
+    from random import randint
 
     num_1 = randint(0, 50)
     num_2 = randint(0, 50)
@@ -112,15 +112,15 @@ def multiply(request):
         old_num_1 = request.POST['old_num_1']
         old_num_2 = request.POST['old_num_2']
 
-        correct_answer = int(old_num_1) - int(old_num_2)
+        correct_answer = int(old_num_1) * int(old_num_2)
         if int(answer) == correct_answer:
-            my_answer = "Correct! " + old_num_1 + " - " + old_num_2 + " = " + answer
+            my_answer = "Correct! " + old_num_1 + " * " + old_num_2 + " = " + answer
             color = "success"
         else:
             my_answer = (
                 "Incorrect! "
                 + old_num_1
-                + " - "
+                + " * "
                 + old_num_2
                 + " Doesn't equal "
                 + answer
@@ -131,7 +131,7 @@ def multiply(request):
 
         return render(
             request,
-            'subtract.html',
+            'multiply.html',
             {
                 'num_1': num_1,
                 'num_2': num_2,
@@ -143,7 +143,7 @@ def multiply(request):
 
     return render(
         request,
-        'subtract.html',
+        'multiply.html',
         {
             'num_1': num_1,
             'num_2': num_2,
@@ -151,4 +151,50 @@ def multiply(request):
     )
 
 def divide(request):
-    return render(request, 'divide.html', {})
+    from random import randint
+
+    num_1 = randint(0, 50)
+    num_2 = randint(0, 50)
+
+    if request.method == "POST":
+        answer = request.POST['answer']
+        old_num_1 = request.POST['old_num_1']
+        old_num_2 = request.POST['old_num_2']
+
+        correct_answer = int(old_num_1) / int(old_num_2)
+        if int(answer) == correct_answer:
+            my_answer = "Correct! " + old_num_1 + " / " + old_num_2 + " = " + answer
+            color = "success"
+        else:
+            my_answer = (
+                "Incorrect! "
+                + old_num_1
+                + " / "
+                + old_num_2
+                + " Doesn't equal "
+                + answer
+                + ", The correct answer is: "
+                + str(correct_answer)
+            )
+            color = "danger"
+
+        return render(
+            request,
+            'divide.html',
+            {
+                'num_1': num_1,
+                'num_2': num_2,
+                'answer': answer,
+                'my_answer': my_answer,
+                'color': color,
+            },
+        )
+
+    return render(
+        request,
+        'divide.html',
+        {
+            'num_1': num_1,
+            'num_2': num_2,
+        },
+    )
