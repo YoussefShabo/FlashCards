38f8a0d6722bac563c0226fad7752d62db03a6cb
YoussefShabo <youssefshabo@gmail.com>
2023-05-25 20:40:22 +0200

    added the subtraction code

--- flash/website/views.py
+++ flash/website/views.py
@@ -6,8 +6,8 @@ def home(request):
 def add(request):
     from random import randint
 
-    num_1 = randint(0,50)
-    num_2 = randint(0,50)
+    num_1 = randint(0, 50)
+    num_2 = randint(0, 50)
 
     if request.method == "POST":
         answer = request.POST['answer']
@@ -19,21 +19,136 @@ def add(request):
             my_answer = "Correct! " + old_num_1 + " + " + old_num_2 + " = " + answer
             color = "success"
         else:
-            my_answer = "Incorrect! " + old_num_1 + " + " + old_num_2 + " Doesn't equal " + answer + ", The correct answer is: " + str(correct_answer)
+            my_answer = (
+                "Incorrect! "
+                + old_num_1
+                + " + "
+                + old_num_2
+                + " Doesn't equal "
+                + answer
+                + ", The correct answer is: "
+                + str(correct_answer)
+            )
             color = "danger"
 
-        return render(request, 'add.html', {'num_1': num_1, 'num_2': num_2, 'answer': answer, 'my_answer': my_answer, 'color': color,})
-    
-    return render(request, 'add.html', {
-        'num_1':num_1,
-        'num_2':num_2,
-        })
+        return render(
+            request,
+            'add.html',
+            {
+                'num_1': num_1,
+                'num_2': num_2,
+                'answer': answer,
+                'my_answer': my_answer,
+                'color': color,
+            },
+        )
+
+    return render(
+        request,
+        'add.html',
+        {
+            'num_1': num_1,
+            'num_2': num_2,
+        },
+    )
 
 def subtract(request):
-    return render(request, 'subtract.html', {})
+    from random import randint
+
+    num_1 = randint(0, 50)
+    num_2 = randint(0, 50)
+
+    if request.method == "POST":
+        answer = request.POST['answer']
+        old_num_1 = request.POST['old_num_1']
+        old_num_2 = request.POST['old_num_2']
+
+        correct_answer = int(old_num_1) - int(old_num_2)
+        if int(answer) == correct_answer:
+            my_answer = "Correct! " + old_num_1 + " - " + old_num_2 + " = " + answer
+            color = "success"
+        else:
+            my_answer = (
+                "Incorrect! "
+                + old_num_1
+                + " - "
+                + old_num_2
+                + " Doesn't equal "
+                + answer
+                + ", The correct answer is: "
+                + str(correct_answer)
+            )
+            color = "danger"
+
+        return render(
+            request,
+            'subtract.html',
+            {
+                'num_1': num_1,
+                'num_2': num_2,
+                'answer': answer,
+                'my_answer': my_answer,
+                'color': color,
+            },
+        )
+
+    return render(
+        request,
+        'subtract.html',
+        {
+            'num_1': num_1,
+            'num_2': num_2,
+        },
+    )
 
 def multiply(request):
-    return render(request, 'multiply.html', {})
+ from random import randint
+
+    num_1 = randint(0, 50)
+    num_2 = randint(0, 50)
+
+    if request.method == "POST":
+        answer = request.POST['answer']
+        old_num_1 = request.POST['old_num_1']
+        old_num_2 = request.POST['old_num_2']
+
+        correct_answer = int(old_num_1) - int(old_num_2)
+        if int(answer) == correct_answer:
+            my_answer = "Correct! " + old_num_1 + " - " + old_num_2 + " = " + answer
+            color = "success"
+        else:
+            my_answer = (
+                "Incorrect! "
+                + old_num_1
+                + " - "
+                + old_num_2
+                + " Doesn't equal "
+                + answer
+                + ", The correct answer is: "
+                + str(correct_answer)
+            )
+            color = "danger"
+
+        return render(
+            request,
+            'subtract.html',
+            {
+                'num_1': num_1,
+                'num_2': num_2,
+                'answer': answer,
+                'my_answer': my_answer,
+                'color': color,
+            },
+        )
+
+    return render(
+        request,
+        'subtract.html',
+        {
+            'num_1': num_1,
+            'num_2': num_2,
+        },
+    )
 
 def divide(request):
     return render(request, 'divide.html', {})
