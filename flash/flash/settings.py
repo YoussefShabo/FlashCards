a6a4846a18618c5bd758fc602fdfd5d5dec827da
YoussefShabo <youssefshabo@gmail.com>
2023-05-26 19:02:49 +0200

    tweaked for Heroku

--- flash/flash/settings.py
+++ flash/flash/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import django_heroku
+import dj_database_url
+
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
