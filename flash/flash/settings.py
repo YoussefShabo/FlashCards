40a9240006e9f13ed07715848843b14d0eede215
YoussefShabo <youssefshabo@gmail.com>
2023-05-26 19:39:47 +0200

    updated styling and reverted the heroku settings

--- flash/flash/settings.py
+++ flash/flash/settings.py
@@ -10,10 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-import django_heroku
-import dj_database_url
 
-from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,8 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'flash.urls'
@@ -129,9 +124,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFileStorage'
-
-django_heroku.settings(locals())
 
 
 
